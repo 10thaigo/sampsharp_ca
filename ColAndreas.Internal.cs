0533e88bf5d6a52700680563ae9d77f66f8f7040
10thaigo <54153816+10thaigo@users.noreply.github.com>
2024-03-31 01:33:21 -0300

    Added dynamic objects

--- ColAndreas.Internal.cs
+++ ColAndreas.Internal.cs
@@ -1,18 +1,33 @@
 ﻿using SampSharp.Core.Natives.NativeObjects;
+using SampSharp.GameMode;
 
 namespace SampSharp.ColAndreas
 {
     public class ColAndreas
     {
         public static ColAndreasInternal Internal { get; } = NativeObjectProxyFactory.CreateInstance<ColAndreasInternal>();
+
+        public static void RemoveBuildingCollision(int modelid, Vector3 position, float radius = 0.50f) => Internal.RemoveBuildingCollision(modelid, position.X, position.Y, position.Z, radius);
     }
 
     public class ColAndreasInternal
     {
         [NativeMethod(Function = "CA_RayCastLine")]
-        public virtual int RayCastLine(float StartX, float StartY, float StartZ, float EndX, float EndY, float EndZ, out float x, out float y, out float z)
-        {
-            throw new NativeNotImplementedException();
-        }
+        public virtual int RayCastLine(float StartX, float StartY, float StartZ, float EndX, float EndY, float EndZ, out float x, out float y, out float z) => throw new NativeNotImplementedException();
+        
+        [NativeMethod(Function = "CA_CreateObject")]
+        public virtual int CreateCollision(int modelid, float x, float y, float z, float rx, float ry, float rz, bool add) => throw new NativeNotImplementedException();
+
+        [NativeMethod(Function = "CA_SetObjectPos")]
+        public virtual int SetCollisionPosition(int index, float x, float y, float z) => throw new NativeNotImplementedException();
+
+        [NativeMethod(Function = "CA_SetObjectRot")]
+        public virtual int SetCollisionRotation(int index, float rx, float ry, float rz) => throw new NativeNotImplementedException();
+
+        [NativeMethod(Function = "CA_DestroyObject")]
+        public virtual int DestroyCollision(int index) => throw new NativeNotImplementedException();
+
+        [NativeMethod(Function = "CA_RemoveBuilding")]
+        public virtual int RemoveBuildingCollision(int modelid, float x, float y, float z, float radius) => throw new NativeNotImplementedException();
     }
 }
---
+++ World/ColDynamicObject.cs
@@ -0,0 +1,56 @@
+﻿using SampSharp.GameMode;
+using SampSharp.GameMode.Definitions;
+using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.World;
+using SampSharp.Streamer.World;
+
+namespace SampSharp.ColAndreas.World
+{
+    public class ColDynamicObject
+    {
+        public int Id { get; }
+        private DynamicObject _dynamicObject;
+        private Vector3 _position;
+        private Vector3 _rotation;
+
+        public Vector3 Position
+        {
+            get => _position;
+            set
+            {
+                ColAndreas.Internal.SetCollisionPosition(Id, value.X, value.Y, value.Z);
+                _dynamicObject.Position = value;
+                _position = value;
+            }
+        }
+
+        public Vector3 Rotation
+        {
+            get => _rotation;
+            set
+            {
+                ColAndreas.Internal.SetCollisionRotation(Id, value.X, value.Y, value.Z);
+                _dynamicObject.Rotation = value;
+                _rotation = value;
+            }
+        }
+
+        public ColDynamicObject(int modelid, Vector3 position, Vector3 rotation = default, int vw = -1, int interior = -1, BasePlayer? player = null, float streamdistance = 200.0f, float drawdistance = 200.0f, DynamicArea? area = null, int priority = 0)
+        {
+            Id = ColAndreas.Internal.CreateCollision(modelid, position.X, position.Y, position.Z, rotation.X, rotation.Y, rotation.Z, false);
+            _position = position;
+            _rotation = rotation;
+            _dynamicObject = new DynamicObject(modelid, position, rotation, vw, interior, player, streamdistance, drawdistance, area, priority);
+        }
+
+        public void SetMaterial(int materialindex, int modelid, string txdname, string texturename, Color materialcolor = new Color()) => _dynamicObject.SetMaterial(materialindex, modelid, txdname, texturename, materialcolor);
+        
+        public void SetMaterialText(int materialindex, string text, ObjectMaterialSize materialsize = ObjectMaterialSize.X256X128, string fontface = "Arial", int fontsize = 24, bool bold = true, Color foreColor = new Color(), Color backColor = new Color(), ObjectMaterialTextAlign textalignment = ObjectMaterialTextAlign.Center) => _dynamicObject.SetMaterialText(materialindex, text, materialsize, fontface, fontsize, bold, foreColor, backColor, textalignment);
+
+        public void Dispose()
+        {
+            ColAndreas.Internal.DestroyCollision(Id);
+            _dynamicObject.Dispose();
+        }
+    }
+}
